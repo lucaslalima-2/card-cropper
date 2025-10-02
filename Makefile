972b0bf17081ecc923c585aea06a570ac254807e
Lucas LaLima <luke@Lucass-MacBook-Pro.local>
2025-10-02 15:34:28 -0400

    Moving towards OpenCV

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
 SCRIPT = crop.py
 INPUT_DIR ?= cards_raw
 OUTPUT_DIR ?= cards_cropped
+VENV=.venv
+PYTHON = .venv/bin/python
 
-setup:
-	pip install -r requirements.txt
+venv:
+	python3 -m venv $(VENV)
+
+setup: venv
+	$(VENV_NAME)/bin/pip install --upgrade pip
+	$(VENV)/bin/pip install -r requirements.txt
 
 run:
-	python $(SCRIPT) $(INPUT_DIR) $(OUTPUT_DIR)
+	$(PYTHON) $(SCRIPT) $(INPUT_DIR) $(OUTPUT_DIR)
 
 clean:
 	rm -rf $(OUTPUT_DIR)/*
--- crop.py
+++ crop.py
@@ -3,10 +3,52 @@ import os, sys
 from PIL import Image
 import numpy as np
 
+def crop_whitespace(jpg):
+  img = Image.open(jpg).convert("RGB")
+  np_img = np.array(img)
+
+  # Convert to grayscale
+  gray = np.mean(np_img, axis=2)
+
+  # Eliminate scan light-bleeding
+  h, w = gray.shape
+  edge_margin = 0.20
+
+  gray_central = gray [
+    int(h * edge_margin):int(h * (1-edge_margin)),
+    int(w * edge_margin):int(w * (1-edge_margin))
+  ]
+
+  # Mask
+  mask = gray_central < 240
+  
+  # Show mask
+  mask_img = Image.fromarray((mask*255).astype(np.uint8))
+  mask_img.show()
+
+  coords = np.argwhere(mask)
+  print(coords)
+  if coords.size == 0:
+    print(f"No content found in {jpg}")
+    return img
+  
+  # Adjusts coords back to full image space
+  y0, x0 = coords.min(axis=0)
+  y1, x1 = coords.max(axis=0) + 1 # includes edge
+  y0 += int(h * edge_margin)
+  y1 += int(h * edge_margin)
+  x0 += int(w * edge_margin)
+  x1 += int(w * edge_margin)
+
+  cropped = img.crop((x0, y0, x1, y1))
+
+  return cropped
+
 def main(indir, outdir):
   # if outdir doesnt exist, make it
-  os.makedires(outdir, exist_ok=True)
+  os.makedirs(outdir, exist_ok=True)
 
+  # Finds all jpgs and sends to crop function
   for root, dirs, files in os.walk(indir):
     for file in files:
       if file.lower().endswith(".jpg"):
